8c8cbfc348009f41323b3929acd76fc17b1989ff
么了 <271707272@qq.com>
2020-09-22 16:00:24 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <Mele>Hi</Mele>
+
+</template>
+
+<script>
+import Mele from './components/Mele.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Mele
+  }
+}
+</script>
